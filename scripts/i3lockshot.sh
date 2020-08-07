da92519a4fb16c29ba4d721c9a0d13a73d325379
greenalien <roshan.rane97@gmail.com>
2020-08-07 15:14:45 +0000

    - minor changes

--- scripts/i3lockshot.sh
+++ scripts/i3lockshot.sh
@@ -1,6 +1,11 @@
 #/bin/sh
 
 image=/tmp/lockshot.png
+if [ image > /dev/null ];
+then
+	rm $image
+	echo image removed.
+fi
 scrot -z $image
-mogrify -blur 0x3 $image
+mogrify -blur 0x15 $image
 i3lock -p default -i $image
