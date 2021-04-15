0654274de72b760a82260ec57ea44e2ac68e9dd7
greenalien <roshan.rane97@gmail.com>
2021-04-15 23:33:13 +0530

    changes added for sources

--- shell-files/.env.sh
+++ shell-files/.env.sh
@@ -1,4 +1,19 @@
+# >>> ls
 export LS_COLORS="$(vivid generate jellybeans)"
+
+# >>> Path
 export PATH=$PATH:$HOME/.local/bin
+
+# >>> ex
 export POSTGRES_USER="greenalien"
 export POSTGRES_PASS="greenAlien@24"
+
+# >>> prompt
+PS1='\e[32m\w\n\e[031m>\e[m '
+
+# >>> qt-gtk theme
+QT_QPA_PLATFORMTHEME=qt5ct
+
+# Ignore These Commands in history
+export HISTIGNORE=${HISTIGNORE:-"shutdown*:halt*:poweroff*:hibernate*:history*"}
+
