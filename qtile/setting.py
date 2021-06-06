b1ed738785849b08550ceafddbfc9a99f03316e1
Roshan Rane <roshan.rane97@gmail.com>
2021-06-06 22:32:40 +0530

    Floating Layout Update

--- qtile/setting.py
+++ qtile/setting.py
@@ -1,4 +1,6 @@
 import os
+from libqtile import layout
+from libqtile.config import Match
 from libqtile.utils import guess_terminal
 from colors import COLORS
 
@@ -21,7 +23,7 @@ bar_config = {
     "size": 25,
     "background": COLORS.BACKGROUND,
     "margin": 0,
-    "opacity": 0.85,
+    "opacity": 1,
     "font": "Space Mono"
 }
 
@@ -49,33 +51,24 @@ workspaces = {
           "matches": None}
               }
 
-floating_rules = [
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'kvms pro.exe'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'Pulseeffects'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'confirmreset'},  # gitk
-    {'wmclass': 'makebranch'},  # gitk
-    {'wmclass': 'maketag'},  # gitk
-    {'wmclass': 'QML Timer'},  # Pilorama
-    {'wmclass': 'polkit-kde-authentication-agent-1'},  # Pilorama
-    {'wmclass': 'kdialog'},  # Pilorama
-    {'wmclass': 'kdesu'},  # Pilorama
-    {'wname': 'branchdialog'},  # gitk
-    {'wname': 'Terminator Preferences'},  # gitk
-    {'wname': 'pinentry'},  # GPG key password entry
-    {'wname': 'Confirm'},  # GPG key password entry
-    {'wname': 'Tip of the Day'},  # GPG key password entry
-    {'wname': 'Confirm Exit'},  # GPG key password entry
-    {'wname': 'Copying files'},  # GPG key password entry
-    {'wname': 'Save File'},  # GPG key password entry
-    {'wname': 'Delete Permanently'},  # GPG key password entry
-    {'wmclass': 'ssh-askpass'},  # ssh-askpass
-]
-
+floating_rules = [*layout.Floating.default_float_rules,
+                  Match(wm_class='Pulseeffects'),
+                  Match(wm_class='kvms pro.exe'),
+                  Match(wm_class='confirmreset'),
+                  Match(wm_class='makebranch'),
+                  Match(wm_class='maketag'),
+                  Match(wm_class='QML Timer'),
+                  Match(wm_class='polkit-kde-authentication-agent-1'),
+                  Match(wm_class='kdialog'),
+                  Match(wm_class='kdesu'),
+                  Match(wm_class='ssh-askpass'),
+                  Match(title='branchdialog'),
+                  Match(title='Terminator Preferences'),
+                  Match(title='pinentry'),
+                  Match(title='Confirm'),
+                  Match(title='Tip of the Day'),
+                  Match(title='Confirm Exit'),
+                  Match(title='Copying files'),
+                  Match(title='Save File'),
+                  Match(title='Delete Permanently'),
+                ]
