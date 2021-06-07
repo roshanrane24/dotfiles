8d098f791634462e0cb5a7220b8989d1d55002ec
Roshan Rane <roshan.rane97@gmail.com>
2021-06-07 16:05:37 +0530

    Go ENV Variables

--- shell-files/env.sh
+++ shell-files/env.sh
@@ -2,18 +2,21 @@
 export LS_COLORS="$(vivid generate jellybeans)"
 
 # >>> Path
-export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/.local/bin:/usr/local/go/bin
 
 # >>> Locale
 export LC_ALL="en_US.UTF-8"
 
 
 # >>> prompt
-PS1='\e[32m\w\n\e[031m>\e[m '
+ export PS1='\e[32m\w\n\e[031m>\e[m '
 
 # >>> qt-gtk theme
-QT_QPA_PLATFORMTHEME=qt5ct
+export QT_QPA_PLATFORMTHEME=qt5ct
 
 # Ignore These Commands in history
 export HISTIGNORE=${HISTIGNORE:-"shutdown*:halt*:poweroff*:hibernate*:history*"}
 
+# GOLANG
+export GOPATH="$HOME/.local/lib/golib"
+export PATH=$PATH:$GOPATH/bin
