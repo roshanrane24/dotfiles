eaa2c0fa39fc5c8b1c85c0d1c3d8af03fa233a3f
Roshan Rane <greenalien@Legion-GA.>
2024-12-22 20:31:43 +0530

    updated shell files

--- shell-files/alias.sh
+++ shell-files/alias.sh
@@ -3,9 +3,20 @@ alias ls='ls --color=auto'
 alias la='ls -la'
 alias lh='ls -lh'
 alias lha='ls -lha'
+alias lha='ls -lha'
 # <<< ls
+#
 # >>> ncdu 
 alias ncdu='ncdu --color dark'
 # <<< ncdu
+#
+# >>> zypper
+alias zp='sudo zypper'
+alias zpr='zp refresh'
+alias zpd='zp remove'
+alias zpi='zp install'
+alias zps='zp search'
+# <<< zypper
+#
 # >>> Alias
 alias vim=nvim
--- shell-files/env.sh
+++ shell-files/env.sh
@@ -1,14 +1,14 @@
+# >>> Path
+export PATH=$PATH:$HOME/.local/bin:/usr/local/go/bin:$HOME/.cargo/bin
+
 # >>> ls
 export LS_COLORS="$(vivid generate jellybeans)"
-
-# >>> Path
-export PATH=$PATH:$HOME/.local/bin:/usr/local/go/bin
 
 # >>> Locale
 export LC_ALL="en_US.UTF-8"
 
 # >>> prompt
- export PS1='\e[32m\w\n\e[031m>\e[m '
+export PS1='\e[32m\w\n\e[031m>\e[m '
 
 # >>> qt-gtk theme
 export QT_QPA_PLATFORMTHEME=qt5ct
@@ -17,14 +17,20 @@ export QT_QPA_PLATFORMTHEME=qt5ct
 export HISTIGNORE=${HISTIGNORE:-"shutdown*:halt*:poweroff*:hibernate*:history*"}
 
 # GOLANG
-export GOROOT=/usr/lib/go
-export GO111MODULE=on
-export GOPATH="$HOME/.local/lib/golib"
-export GOPATH="$HOME/Build/gocode"
-export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+# export GOROOT=/usr/lib/go
+# export GO111MODULE=on
+# export GOPATH="$HOME/.local/lib/golib"
+# export GOPATH="$HOME/Build/gocode"
+# export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 
 # EDITOR
 export EDITOR=vim
 
+# DOTFILES
+export DOTFILES_PATH='~/.config/dotfiles'
+
 # JAVA
-export JAVA_HOME=/usr/lib/jvm/java-16-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+
+# TEMP
+export TMUX_TMPDIR='/tmp'
